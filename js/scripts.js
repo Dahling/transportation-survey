a76792a13a510c8658ba121c404d707f787e9caa
Ian Garner and Jenna Cooper <ian+jenna@>
2017-05-02 15:23:22 -0700

    add more focused comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,13 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
 // the above will link jquery with the front end to pull from the <form id in the html
     event.preventDefault();
-    // this will cause the answers to display on the same page
+    // ^this will cause the answers to display on the same page
     $("#work-responses").show();
-    // this will make the responses show from the <span>
-    $("input:checkbox[name=work-transportation]:checked").each(function()
+    // ^this will make the responses show from the <span>
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
     // the above refers to the input being placed into an array[]. checked makes things more specific.
-    {
     var workTransportationMode = $(this).val();
-    // workTransportationMode is a random name to refer to. $(this) gives reference to the above and tells what the above statements will become below.
+    // ^this line takes the value of ("input:checkboxblahblahblah") and stores it in the variable named workTransportationMode.
     $('#work-responses').append(workTransportationMode + "<br>");
     });
     $("#fun-responses").show();
