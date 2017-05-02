8cd2031ca410911e8ebede66885d509feda3fccc
Ian Garner and Jenna Cooper <ian+jenna@>
2017-05-02 15:14:46 -0700

    Add fun transportation options

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
 // the above will link jquery with the front end to pull from the <form id in the html
-
     event.preventDefault();
     // this will cause the answers to display on the same page
     $("#work-responses").show();
@@ -13,6 +12,11 @@ $(document).ready(function(){
     var workTransportationMode = $(this).val();
     // workTransportationMode is a random name to refer to. $(this) gives reference to the above and tells what the above statements will become below.
     $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
     });
     $('#transportation_survey').hide();
     // the above will hide the survey after the responses are submitted to only show the responses on the page.
